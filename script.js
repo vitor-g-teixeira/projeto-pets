443fc14fff26d96ca7255530b4a9c023a57e5c7b
Leonardo Costa <rodeghieroleonardo@gmail.com>
2024-11-02 18:31:34 -0300

    commit pra não conflitar

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 class Usuario{
-    constructor(){
+    constructor(nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento){
         this.id_usuario;
-        this.nome;
-        this.sobrenome;
-        this.telefone;
-        this.email;
-        this.cidade;
-        this.bairro;
-        this.logradouro;
-        this.numero;
-        this.complemento;
-        this.cep;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
         this.aceita_necessidade;
         this.aceita_doenca;
         this.aceita_exclusividade;
@@ -70,8 +70,38 @@ class Pet_Necessidade{
     }	
 }
 
-const usuario1 = new Usuario();
-usuario1.nome = 'Paulo';
-usuario1.cpf = '999.999.999-99';
-console.log(usuario1);
+
+
+function cadastrar(){
+    
+    const campos = document.getElementsByClassName('campo');
+    const valores = Array.from(campos).map(campo => campo.value);
+
+    const [nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento] = valores;
+
+
+
+    const usuario = new Usuario(nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento);
+    
+
+    //Entrada de dados está funcionando. Agr precisa validar esses dados e inserir no banco de dados
+
+
+
+    document.getElementById("teste").innerHTML = `
+    <p>Nome: ${usuario.nome} 
+    <p>Sobrenome: ${usuario.sobrenome}
+    <p>telefone: ${usuario.telefone}
+    <p>Email: ${usuario.email} 
+    <p>Senha: ${usuario.senha}
+    <p>estado: ${usuario.estado} 
+    <p>cidade: ${usuario.cidade} 
+    <p>logradouro: ${usuario.logradouro}
+    <p>número: ${usuario.numero}
+    <p>complemento: ${usuario.complemento}
+    `;
+
+    
+    
+}
 
