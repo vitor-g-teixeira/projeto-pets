452d2d71d6523ee54bc177ade77929a703642a0e
Leonardo Costa Rodeghiero <23121024@faculdadedombosco.edu.br>
2024-11-13 19:33:53 -0300

    Agr tem que funcionar o Banco

---
+++ TesteDB/db.js
@@ -0,0 +1,30 @@
+import pg from 'pg';
+const { Client } = pg;
+export async function executeQuery(query, params) {
+    const client = new Client({
+        user: 'postgres',
+        password: 'MABD',
+        host: 'localhost',
+        port: 5432,
+        database: 'MeAdota',
+    });
+    await client.connect();
+    try {
+        await client.query(query, params);
+    } catch (err) {
+        console.error("Erro ao executar a query:", err);
+        throw err;
+    } finally {
+        await client.end();
+    }
+}
+export async function CadastroProduto(nome, email, senha, data_nascimento, rua, numero, bairro, cidade, estado, cep) {
+    const query = "INSERT INTO produtos(nome, email, senha, data_nascimento, rua, numero, bairro, cidade, estado, cep) VALUES($1, $2, $3, $4, 5, $6, $7, $8, $9, $10)";
+    const params = [nome, email, senha, data_nascimento, rua, numero, bairro, cidade, estado, cep];
+    try {
+        await executeQuery(query, params);
+        console.log("Usuário cadastrado com sucesso!");
+    } catch (err) {
+        console.error("Erro ao cadastrar usuário:", err);
+    }
+}
