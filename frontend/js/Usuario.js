7577bad6c455d0f5d8022e18fc75f108df9bf6f8
Leonardo Costa <rodeghieroleonardo@gmail.com>
2024-11-19 15:59:08 -0300

    tentando validar a senha

--- frontend/js/Usuario.js
+++ frontend/js/Usuario.js
@@ -23,14 +23,10 @@ class Usuario{
 
 }
 
-
-/*async function cadastrar(){
-    
     const nome = document.getElementById("nome").value.trim();
     const sobrenome = document.getElementById("sobrenome").value.trim();
     const email = document.getElementById("email").value.trim();
     const telefone = document.getElementById("telefone").value.trim();
-    const senha = document.getElementById("senha").value.trim();
 
     const cidade = document.getElementById("cidade").value.trim();
     const estado = document.getElementById("estado").value.trim();
@@ -40,63 +36,7 @@ class Usuario{
 
     const valores = [nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento];
 
-    function validarDados(){
-
-        let contCampo = 0;
-        for(let cont = 0; cont < valores.length; cont++)
-        {
-            if(valores[cont] === '')
-            {
-                contCampo++;
-            }
-        }
-
-        if(contCampo > 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-    }
     
-    
-        if(validarDados() == true && validarEmail(email))
-            {
-                const usuario = new Usuario(nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento);
-                alert("OK");
-            }
-            else{
-                alert("Preencha todos os campos!");
-            }
-        
-    
-    
-}
-
-function validarEmail(email){
-    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if(regexEmail.test(email)){
-        return true;
-    }
-    return false;
-}*/
-
-
-    
-    const nome = document.getElementById("nome").value.trim();
-    const sobrenome = document.getElementById("sobrenome").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const telefone = document.getElementById("telefone").value.trim();
-    const senha = document.getElementById("senha").value.trim();
-
-    const cidade = document.getElementById("cidade").value.trim();
-    const estado = document.getElementById("estado").value.trim();
-    const logradouro = document.getElementById("logradouro").value.trim();
-    const numero = document.getElementById("numero").value.trim();
-    const complemento = document.getElementById("complemento").value.trim();
 
     //valores para completar
     const data_nascimento = "2000-01-01";
@@ -104,77 +44,82 @@ function validarEmail(email){
     const bairro = "Bairro 1234";
     const cep = "85240-001";
 
-
-
-    const valores = [nome, sobrenome, email, telefone, senha, cidade, estado, logradouro, numero, complemento];
-
-    function validarDados(){
-
-        return true;
-    }
     
-    
-        if(validarEmail(email))
-            {
-                const response = await fetch('http://localhost:3000/inserirUsuario', { 
-                    method: 'POST', 
-                    headers: { 
-                        'Content-Type': 'application/json' 
-                    }, 
-                    body: JSON.stringify({ nome, email, senha, data_nascimento, rua, numero, bairro, cidade, estado, cep }) 
-                });
 
-                if (response.ok) 
-                    { 
-                        alert('Dados inseridos com sucesso!'); 
-                    } 
-                    else 
-                    { 
-                        alert('Erro ao inserir dados.');
-                    }
-            }
-            
+    if(validarEmail(email) && validarSenha(senha))
+        {
+            const response = await fetch('http://localhost:3000/inserirUsuario', { 
+                method: 'POST', 
+                headers: { 
+                    'Content-Type': 'application/json' 
+                }, 
+                body: JSON.stringify({ nome, email, senha, data_nascimento, rua, numero, bairro, cidade, estado, cep }) 
+            });
+    
+            if (response.ok) 
+                { 
+                    alert('Dados inseridos com sucesso!'); 
+                } 
+                else 
+                { 
+                    alert('Erro ao inserir dados.');
+                }
+        }
+    
+
+        function validarEmail(email){
+            const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         
-    
-    
-
-
-function validarEmail(email){
-    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if(regexEmail.test(email)){
-        return true;
-    }
-    return false;
+            if(regexEmail.test(email)){
+                return true;
+            }
+            else{
+                alert("email inválido");
+                return false;
+            }
+        }
+        
+function validarDados(event)
+{
+    let senha = document.getElementById('senha').value.trim();
+    const errorMessage = document.getElementById('error-message');
+          
+    // Expressão regular para validar a senha
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+          
+              // Validar a senha
+              if (!regex.test(senha)) {
+                // Exibe mensagem de erro se a senha não for válida
+                errorMessage.innerHTML = 'A senha deve ter pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula e um número.';
+                // Impede a submissão do formulário
+                return false;
+              }
+          
+              // Limpa a mensagem de erro se a senha for válida
+              errorMessage.innerHTML = '';
+              return true;
 }
-
-
-
-
-// Adiciona o evento de submit ao formulário após o DOM ser carregado 
-document.addEventListener('DOMContentLoaded', function () { 
-    document.getElementById('formulario').addEventListener('submit', ValidarSenha); 
+        
+document.addEventListener('DOMContentLoaded', function () {
+     const formulario = document.getElementById('formulario');
+          
+    formulario.addEventListener('submit', validarDados);
 });
 
-function ValidarSenha(event){
-    senha = document.getElementById('senha').value; 
-    const errorMessage = document.getElementById('error-message')
-    // Expressão regular para validar a senha 
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; 
-    if (regex.test(senha)) 
-        { 
-             // Limpa a mensagem de erro se a senha for válida 
-            errorMessage.innerHTML = ''; 
-            return true;
-        } 
-    else {
+    
+
+
+    
         
-        // Exibe mensagem de erro se a senha não for válida 
-        errorMessage.innerHTML = 'A senha deve ter pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula e um número.'; 
-        event.preventDefault();
-        return false; 
-    }       
             
-}
+        
+    
+    
+
+
+
+
+
+
 
 
